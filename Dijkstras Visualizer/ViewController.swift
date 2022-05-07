a57b3643cdcab0d47e9c437bc9283796426a09d3
Spencer Kinsey-Korzym <huhu728@gmail.com>
2022-05-07 17:41:08 -0400

    Initial Commit

---
+++ Dijkstras Visualizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dijkstras Visualizer
+//
+//  Created by Spencer Kinsey-Korzym on 5/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
